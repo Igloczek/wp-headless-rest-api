b802d449c9bed4aee329321ba1880a4adf63cdf3
Bartek Igielski <igloczek@gmail.com>
2017-07-03 01:53:49 +0200

    Methods rewrite. Custom data sanitization added.

--- wp-headless-rest-api.php
+++ wp-headless-rest-api.php
@@ -9,21 +9,31 @@ License: GPLv3
 Licence URI: https://www.gnu.org/licenses/gpl.html
 */
 
-class Headless_REST_Controller
+include plugin_dir_path( __FILE__ ) . 'url-to-query.php';
+
+class Headless_REST_Controller extends WP_REST_Posts_Controller
 {
     public function __construct () {
         $this->namespace = '/headless/v1';
+        $this->types = [
+            'author',
+            'category',
+            'page',
+            'single',
+            'tag',
+            'tax'
+        ];
     }
 
     public function register_routes () {
-        register_rest_route( $this->namespace, '/type-by-url/(?P<url>\S+)', array(
+        register_rest_route( $this->namespace, '/url_to_query/(?P<url>\S+)', array(
             'methods'  => 'GET',
-            'callback' => array( $this, 'get_type_by_url' )
+            'callback' => array( $this, 'url_to_query' )
         ));
 
-        register_rest_route( $this->namespace, '/post-by-url/(?P<url>\S+)', array(
+        register_rest_route( $this->namespace, '/home_page', array(
             'methods'  => 'GET',
-            'callback' => array( $this, 'get_post_by_url' )
+            'callback' => array( $this, 'get_home_page' )
         ));
 
         register_rest_route( $this->namespace, '/menu/(?P<id>[a-zA-Z(-]+)', array(
@@ -38,39 +48,75 @@ class Headless_REST_Controller
     * @param WP_REST_Request $request Current request.
     * @return string Content type
     */
-    public function get_type_by_url ( $request ) {
-        $type = get_post_type( url_to_postid( $request['url'] ) );
+    public function url_to_query ( $request ) {
+        $url = esc_sql($request['url']);
+        $posts = new WP_Query( url_to_query( $url ) );
 
-        if ( empty( $type ) ) {
+        if ( !$posts->have_posts() ) {
             return new WP_Error(
                 'no_post',
-                'Invalid url to post',
+                'Invalid url',
                 array( 'status' => 404 )
             );
         }
 
-        return $type;
+        $data = array();
+
+        foreach ( $posts->posts as $post ) {
+            $response = $this->prepare_item_for_response( $post, $request );
+            $data[] = $this->prepare_response_for_collection( $response );
+        }
+
+        $reponse = new stdClass();
+
+        foreach ($this->types as $type) {
+            if ( $posts->{'is_' . $type} ) {
+                $reponse->type = $type;
+            }
+        }
+
+        $reponse->posts = $data;
+
+        return $reponse;
     }
 
-
     /**
-    * Get the post object by URL
+    * Get front page content
     *
     * @param WP_REST_Request $request Current request.
-    * @return array Post object
+    * @return string Content type
     */
-    public function get_post_by_url ( $request ) {
-        $post = get_post( url_to_postid( $request['url'] ) );
+    public function get_home_page ( $request ) {
+        $posts;
 
-        if ( empty( $post ) ) {
+        if (get_option('page_on_front') > 0) {
+            $posts = new WP_Query( array( 'page_id' => get_option('page_on_front') ) );
+        }
+        else {
+            $posts = new WP_Query( array( 'post_type' => 'post' ) );
+        }
+
+        if ( !$posts->have_posts() ) {
             return new WP_Error(
                 'no_post',
-                'Invalid url to post',
+                'Invalid url',
                 array( 'status' => 404 )
             );
         }
 
-        return $post;
+        $data = array();
+
+        foreach ( $posts->posts as $post ) {
+            $response = $this->prepare_item_for_response( $post, $request );
+            $data[] = $this->prepare_response_for_collection( $response );
+        }
+
+        $reponse = new stdClass();
+
+        $reponse->type = 'home';
+        $reponse->posts = $data;
+
+        return $reponse;
     }
 
     /**
@@ -92,6 +138,89 @@ class Headless_REST_Controller
 
         return $menu;
     }
+
+	/**
+	 * Prepares a single post output for response.
+	 *
+	 * @since 4.7.0
+	 * @access public
+	 *
+	 * @param WP_Post         $post    Post object.
+	 * @param WP_REST_Request $request Request object.
+	 * @return WP_REST_Response Response object.
+	 */
+	public function prepare_item_for_response( $post, $request ) {
+		$GLOBALS['post'] = $post;
+
+		setup_postdata( $post );
+
+		// Base fields for every post.
+		$data = array();
+
+        $data['id'] = $post->ID;
+        $data['date'] = $this->prepare_date_response( $post->post_date_gmt, $post->post_date );
+
+
+        $data['modified'] = $this->prepare_date_response( $post->post_modified_gmt, $post->post_modified );
+
+        $data['slug'] = $post->post_name;
+        $data['status'] = $post->post_status;
+        $data['type'] = $post->post_type;
+        $data['link'] = get_permalink( $post->ID );
+        $data['title'] = get_the_title( $post->ID );
+        $data['content'] = apply_filters( 'the_content', $post->post_content );
+        $data['excerpt'] = apply_filters( 'the_excerpt', apply_filters( 'get_the_excerpt', $post->post_excerpt, $post ) );
+        $data['author'] = (int) $post->post_author;
+        $data['featured_media'] = (int) get_post_thumbnail_id( $post->ID );
+        $data['parent'] = (int) $post->post_parent;
+        $data['comment_status'] = $post->comment_status;
+        $data['sticky'] = is_sticky( $post->ID );
+        if ( $template = get_page_template_slug( $post->ID ) ) {
+            $data['template'] = $template;
+        } else {
+            $data['template'] = '';
+        }
+
+        $data['format'] = get_post_format( $post->ID );
+
+        // Fill in blank post format.
+        if ( empty( $data['format'] ) ) {
+            $data['format'] = 'standard';
+        }
+
+		$taxonomies = wp_list_filter(
+            get_object_taxonomies( $this->post_type, 'objects' ),
+            array( 'show_in_rest' => true )
+        );
+
+		foreach ( $taxonomies as $taxonomy ) {
+			$base = ! empty( $taxonomy->rest_base ) ? $taxonomy->rest_base : $taxonomy->name;
+            $terms = get_the_terms( $post, $taxonomy->name );
+            $data[ $base ] = $terms ? array_values( wp_list_pluck( $terms, 'term_id' ) ) : array();
+		}
+
+		$context = ! empty( $request['context'] ) ? $request['context'] : 'view';
+		$data    = $this->add_additional_fields_to_object( $data, $request );
+		$data    = $this->filter_response_by_context( $data, $context );
+
+		// Wrap the data in a response object.
+		$response = rest_ensure_response( $data );
+
+		$response->add_links( $this->prepare_links( $post ) );
+
+		/**
+		 * Filters the post data for a response.
+		 *
+		 * The dynamic portion of the hook name, `$this->post_type`, refers to the post type slug.
+		 *
+		 * @since 4.7.0
+		 *
+		 * @param WP_REST_Response $response The response object.
+		 * @param WP_Post          $post     Post object.
+		 * @param WP_REST_Request  $request  Request object.
+		 */
+		return apply_filters( "rest_prepare_{$this->post_type}", $response, $post, $request );
+	}
 }
 
 function headless_register_rest_routes () {
